d5e607f90b7f7622cdb82e7ce4f02fe85e97165a
iurii-kyrylenko <yuriproject@gmail.com>
2024-05-09 13:21:56 +0200

    Deploy to gh pages

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000",
+    // uri: "http://localhost:4000",
+    uri: "https://hobbies-graphql.onrender.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
