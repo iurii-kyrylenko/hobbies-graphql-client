343ba2c0683bec99f6bb1e27630efd2dff984391
iurii-kyrylenko <yuriproject@gmail.com>
2024-05-19 16:23:07 +0200

    Handle error at loading settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,10 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useMutation, useQuery } from "@apollo/client";
+import { ApolloError, useMutation, useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import { AppDispatch, RootState } from "../store";
 import { openSnackbar } from "../store/app-slice";
 import SettingsForm, { SettingsData, SubmitData } from "../components/SettingsForm";
-import { useEffect } from "react";
 import { GET_USER, UPDATE_USER } from "../queries/users";
 
 interface QueryData {
@@ -20,29 +19,27 @@ const Settings = () => {
     const dispatch: AppDispatch = useDispatch();
     const navigate = useNavigate();
 
+    const handleError = (error: ApolloError) =>
+        dispatch(openSnackbar({ message: error.message, severity: "error" }));
+
+    const handleSuccess = () => {
+        navigate("/");
+        dispatch(openSnackbar({ message: "Settings updated!", severity: "success" }));
+    };
+
     const { data } = useQuery<QueryData, QueryVars>(GET_USER, {
         variables: { id: userId },
+        onError: handleError,
         fetchPolicy:  "network-only", // to have recent data
     });
 
-    const [updateUser, { data: updateResult, error }] = useMutation(UPDATE_USER);
-
-    useEffect(() => {
-        if (error) {
-            dispatch(openSnackbar({ message: error.message, severity: "error" })); 
-        }
-        if (updateResult) {
-            navigate("/");
-            dispatch(openSnackbar({ message: "Settings updated!", severity: "success" })); 
-        }
-    }, [updateResult, error]);
+    const [updateUser] = useMutation(UPDATE_USER);
 
     const handleSubmit = (submitData: SubmitData) => {
         updateUser({
             variables: { id: userId, settings: submitData },
-            onError(error) {
-                dispatch(openSnackbar({ message: error.message, severity: "error" }));
-            },
+            onError: handleError,
+            onCompleted: handleSuccess,
         });
     };
 
